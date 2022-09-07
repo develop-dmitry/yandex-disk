6dd111a848afbce29f45caec3c6350c13da8dc5e
Козлов Дмитрий <develop.dmitry@gmail.com>
2022-09-07 23:59:38 +0300

    Initial frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const {VueLoaderPlugin} = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: '3000',
+            server: {
+                baseDir: ['dist']
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.vue$/,
+                use: [
+                    'vue-loader'
+                ]
+            },
+        ]
+    }
+}
