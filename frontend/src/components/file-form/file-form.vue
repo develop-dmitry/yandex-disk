ee4c37c3b7a2ec16cbd33c96612f12064064c633
dmitriy kozlov <d.kozlov@intensa.ru>
2022-09-09 15:44:02 +0300

    Visual part of the app

--- frontend/src/components/file-form/file-form.vue
+++ frontend/src/components/file-form/file-form.vue
@@ -2,6 +2,8 @@
   <form action="#">
     <button class="button" @click="selectFile">{{ fileText }}</button>
     <input type="file" id="file" ref="file" v-on:change="fileUpload()" v-show="false">
+    <div class="form-error" data-role="form-error" v-show="isShowMessage">Ошибка</div>
+    <button class="button button__margin" v-show="file">Загрузить</button>
   </form>
 </template>
 
@@ -12,24 +14,57 @@
     name: "file-form",
     data: () => {
       return {
-        isOpen: false,
+        isShowMessage: false,
         file: "",
-        fileText: "Загрузить файл"
+        fileText: "",
+        excludeExtension: [".exe", ".sh"]
       }
     },
     methods: {
       selectFile() {
-        console.log(document.querySelector("#file").value);
         document.querySelector("#file").click();
       },
       fileUpload() {
-        this.file = this.$refs.file.files[0];
-      }
-    },
-    watch: {
-      file: function () {
+        this.hideMessage();
+        if (this.checkFile(this.$refs.file.files[0].name)) {
+          this.file = this.$refs.file.files[0];
+          this.setButtonTextAsFileName();
+        } else {
+          this.showMessage("Запрещенное расширение файла");
+          this.resetFile();
+        }
+      },
+      checkFile(fileName) {
+        let res = false;
+        let extension = fileName.match(/\.\w+$/i);
+        if (extension != null) {
+          if (this.excludeExtension.indexOf(extension[0]) === -1) {
+            res = true;
+          }
+        }
+        return res;
+      },
+      showMessage(message) {
+        this.isShowMessage = true;
+        document.querySelector("[data-role=form-error]").innerText = message;
+      },
+      hideMessage() {
+        this.isShowMessage = false;
+      },
+      resetFile() {
+        document.querySelector("#file").value = "";
+        this.file = "";
+        this.setButtonDefaultText();
+      },
+      setButtonDefaultText() {
+        this.fileText = "Выбрать файл";
+      },
+      setButtonTextAsFileName() {
         this.fileText = this.file.name;
       }
+    },
+    created() {
+      this.setButtonDefaultText();
     }
   }
 </script>
